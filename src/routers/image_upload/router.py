f17d72a55a75e518e14122400fa455ef8207adee
Amit4218 <amitbhagat621@gmail.com>
2026-05-30 17:12:28 +0530

    remove the test route

--- src/routers/image_upload/router.py
+++ src/routers/image_upload/router.py
@@ -10,59 +10,53 @@ import shutil
 image_upload_router = APIRouter(prefix="/api/upload", tags=["image_upload"])
 
 
-@image_upload_router.post("/test")
-async def upload_images(images: list[UploadFile] = File(...)):
-    print(len(images))
-    return {"uploaded": True}
-
-
 @image_upload_router.post("/{bucket_id}")
 async def upload_image_to_bucket(
         bucket_id: str,
-        h: int = None,
-        w: int = None,
-        ext: str = "webp",
-        file: UploadFile = File(...),
+        files: list[UploadFile] = File(...),
         db: Session = Depends(get_db)
 
 ) -> ImageUploadSuccess:
 
-    if not file:
+    if not files:
         raise HTTPException(status_code=404, detail="file not found")
+    
+    # get data for the bucket config
+    result = db.query(BucketConfig).filter_by(
+        public_access_id=bucket_id).first()
+
+    if not result:
+        raise HTTPException(
+            status_code=400, detail="no bucket found!")
 
     try:
         os.makedirs("uploads", exist_ok=True)
+        
+        for file in files:
 
-        save_path = f"uploads/{file.filename}"
+            save_path = f"uploads/{file.filename}"
 
-        with open(save_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+            with open(save_path, "wb") as buffer:
+                shutil.copyfileobj(file.file, buffer)
 
-        # get data for the bucket config
-        result = db.query(BucketConfig).filter_by(
-            public_access_id=bucket_id).first()
 
-        if not result:
-            raise HTTPException(
-                status_code=400, detail="no bucket found!")
+            # # process the images
+            # current_process_image = ProcessImage(file_path=save_path)
 
-        # process the images
-        current_process_image = ProcessImage(file_path=save_path)
+            # # image resize
+            # if result.w and resulh:
+            #     current_process_image.resize_image(height=h, width=w)
 
-        # image resize
-        if w and h:
-            current_process_image.resize_image(height=h, width=w)
+            # # change file ext default: webp
+            # current_process_image.change_file_extension(ext=ext)
 
-        # change file ext default: webp
-        current_process_image.change_file_extension(ext=ext)
+            # # compress the image
+            # current_process_image.compress_image()
 
-        # compress the image
-        current_process_image.compress_image()
+            # # save the image
+            # current_process_image.save()
 
-        # save the image
-        current_process_image.save()
-
-        # upload the image to the cloud bucket
+            # upload the image to the cloud bucket
 
         return ImageUploadSuccess(
             success=True,
