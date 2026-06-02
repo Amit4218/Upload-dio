0e4843159ebb74c5755107172babdb795dddbfb7
Amit4218 <amitbhagat621@gmail.com>
2026-06-02 21:33:07 +0530

    updated the bucket creation schema

--- src/routers/dashboard/schemas/dashnoard.py
+++ src/routers/dashboard/schemas/dashnoard.py
@@ -1,11 +1,34 @@
 from pydantic import BaseModel
-from typing import  Literal
+from typing import  Literal, Union
 
 BucketProviderType = Literal["AWS", "CLOUDFLARE", "CLOUDINARY","IMAGEKIT"]
 EXT_TYPE = Literal["png", "jpeg", "webp"]
 
 
+class CloudinaryConfig(BaseModel):
+    cloud_name: str
+    api_key: str
+    api_secret: str
+    
+
+class ImageKitConfig(BaseModel):
+    public_key: str
+    private_key: str
+    url_endpoint: str
+    
+class AWSConfig(BaseModel):
+    access_id: str
+    access_secret: str
+    bucket_url: str
+
+class CloudflareConfig(BaseModel):
+    access_id: str
+    access_secret: str
+    bucket_url: str
+
+
 class Resize(BaseModel):
+    resize_image: bool = False
     height: int
     width: int
 
@@ -21,10 +44,8 @@ class BucketImageSettings(BaseModel):
 class CreateBucketConfig(BaseModel):
     bucket_provider: BucketProviderType
     bucket_name: str
-    access_id: str
-    access_secret: str
+    provider_config: Union[AWSConfig, CloudflareConfig, CloudinaryConfig, ImageKitConfig]
     allowed_origin: str
-    bucket_url: str
     image_settings: BucketImageSettings
 
 
