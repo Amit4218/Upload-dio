6fcd835d51a84ebdbc4334d356a11e04a0e2e210
Amit4218 <amitbhagat621@gmail.com>
2026-06-02 21:32:31 +0530

    updated the bucket_config model

--- models/bucket_config.py
+++ models/bucket_config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Literal, List, Optional
+from typing import Literal, List, Optional, Union
 
 
 BucketProviderType = Literal["AWS", "CLOUDFLARE", "CLOUDINARY","IMAGEKIT"]
@@ -7,6 +7,7 @@ EXT_TYPE = Literal["png", "jpeg", "webp"]
 
 
 class Resize(BaseModel):
+    resize_image: bool = False
     height: int
     width: int
 
@@ -23,13 +24,33 @@ class Images(BaseModel):
     filename:str
     url:str
 
+
+class CloudinaryConfig(BaseModel):
+    cloud_name: str
+    api_key: str
+    api_secret: str
+    
+
+class ImageKitConfig(BaseModel):
+    public_key: str
+    private_key: str
+    url_endpoint: str
+    
+class AWSConfig(BaseModel):
+    access_id: str
+    access_secret: str
+    bucket_url: str
+
+class CloudflareConfig(BaseModel):
+    access_id: str
+    access_secret: str
+    bucket_url: str
+
 class BucketConfig(BaseModel):
     bucket_provider: BucketProviderType
     bucket_name: str
-    access_id: str
-    access_secret: str
+    provider_config: Union[AWSConfig, CloudflareConfig, CloudinaryConfig, ImageKitConfig]
     allowed_origin: str
-    bucket_url: str
     public_access_id:str
     image_settings: BucketImageSettings
     images:Optional[List[Images]] = None
