69f6460115670cafa5da92f9ed8e6e77e02aa56e
Amit4218 <amitbhagat621@gmail.com>
2026-06-25 12:52:57 +0530

    added the settings to get the variables from env file and a db to connect with mongodb

---
+++ server/src/config/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL:str
+    ADMIN_USERNAME:str
+    ADMIN_PASSWORD:str
+    SECRET_KEY:str
+    ALGORITHM:str
+    ACCESS_TOKEN_EXPIRE_MINUTES:int
+    
+    
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings() # pyright: ignore[reportCallIssue]
