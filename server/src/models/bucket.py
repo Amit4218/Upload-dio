cb6b406d9ea5dc9e9a0c81884f1fb4f9b37eb342
Amit4218 <amitbhagat621@gmail.com>
2026-07-09 21:57:58 +0530

    added a provider to hold credientials for external buckets

--- server/src/models/bucket.py
+++ server/src/models/bucket.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-from typing import Literal, List
+from typing import Literal, List, Optional
 
 
 EXT_TYPE = Literal["png", "jpeg", "webp"]
-BUCKET_HOST  = Literal["server"]
+BUCKET_HOST  = Literal["server", "s3"]
+PROVIDERS = Literal["amazon", "cloudflare"]
 
 
 class Resize(BaseModel):
@@ -23,12 +24,23 @@ class BucketImageSettings(BaseModel):
     change_file_ext: ChangeFileExtention
 
 
+class S3Credientials(BaseModel):
+    ACCESS_KEY_ID:str
+    SECRET_ACCESS_KEY:str
+    BUCKET_REGION:str
+    BUCKET_NAME:str
+    
+    
+class BucketProvider(BaseModel):
+    provider_name:PROVIDERS
+    credientials:S3Credientials
+
 class BucketConfig(BaseModel):
     bucket_host: BUCKET_HOST = "server"
     bucket_name: str
     allowed_origin: str
     maxfiles: int = 1
-    save_folder_path:str
+    save_folder_path:str = "temp"
     public_access_id:str
     include_videos: bool = False
     include_files: bool = False
@@ -36,3 +48,4 @@ class BucketConfig(BaseModel):
     callback_url: str | None = None
     image_settings: BucketImageSettings
     images:List[str] = []
+    providers: Optional[BucketProvider] = None
