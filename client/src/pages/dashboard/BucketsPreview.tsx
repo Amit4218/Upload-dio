5f0c01ead86257288f0dc14ca2a25826eca8c5b1
Amit4218 <amitbhagat621@gmail.com>
2026-07-21 19:33:29 +0530

    chore: added the Bucket preview card to the bucket preview page

--- client/src/pages/dashboard/BucketsPreview.tsx
+++ client/src/pages/dashboard/BucketsPreview.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import UploadConfigModal from "@/components/CreateBucket";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import BucketPreviewCard from "@/components/BucketPreviewCard";
 
 function BucketsPreview() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,15 +13,17 @@ function BucketsPreview() {
       <div className="flex justify-between items-center h-10 pb-5 border-b">
         <Label>Bucket Previews</Label>
 
-        <Button onClick={() => setIsModalOpen(true)}>
-          Create New Bucket
-        </Button>
+        <Button onClick={() => setIsModalOpen(true)}>Create New Bucket</Button>
       </div>
 
       <UploadConfigModal
         open={isModalOpen}
         onClose={() => setIsModalOpen(false)}
       />
+
+      <ScrollArea className="max-h-[83vh] overflow-y-auto mt-5">
+        <BucketPreviewCard />
+      </ScrollArea>
     </>
   );
 }
